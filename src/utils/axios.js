b6d6247347f0551e96dbc22a79b3dca2c667fa15
saifakib <45618498+saifakib@users.noreply.github.com>
2021-03-18 00:17:33 +0600

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -4,4 +4,7 @@ const instance = axios.create({
     baseURL: 'https://jobs.github.com/positions.json'
 })
 
+instance.defaults.headers.common["Access-Control-Allow-Origin","Access-Control-Allow-Headers"] = ["*","Origin, X-Requested-With, Content-Type, Accept"];
+
+
 export default instance;
